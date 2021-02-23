ca10d156023f4188bf54f66d4c3d6becc5f92270
Luiz <69827778+Lu1zReis@users.noreply.github.com>
2021-02-23 10:21:27 -0400

    Delete script_050.py

--- testes e exercícios/exercicios/script_050.py
+++
@@ -1,9 +0,0 @@
-x = 0
-quant = 0
-for c in range(0, 6):
-    valor = int(input())
-    if(valor % 2 == 0):
-        x += valor
-        quant += 1
-
-print('O total de pares são: {}\nE a soma é: {}'.format(quant, x))
