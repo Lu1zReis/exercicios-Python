64bedd0591c1f7482bd9526bb3a27058fb86e0be
Luiz <69827778+Lu1zReis@users.noreply.github.com>
2021-02-23 10:21:40 -0400

    Delete script_051.py

--- testes e exercícios/exercicios/script_051.py
+++
@@ -1,7 +0,0 @@
-PrimeiroTermo = int(input('Digite o primeiro termo: '))
-Razao = int(input('Digite a sua razão: '))
-for c in range(0, 10):
-    valor = PrimeiroTermo + (Razao * c)
-    print('{}'.format(valor), end=' -> ')
-print('ACABOU')
-  
