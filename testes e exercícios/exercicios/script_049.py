06c2a155bb7725d967124e179a3baf9c67b9163a
Luiz <eluiz8204@gmail.com>
2021-02-23 10:22:28 -0400

    Update script_049.py

--- testes e exercícios/exercicios/script_049.py
+++ testes e exercícios/exercicios/script_049.py
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 x = int(input('Adicione um valor para ver a sua tabuada: '))
 for c in range(1, 11):
     input('{} x {} = {}'.format(x, c, x*c))
+=======
+x = int(input('Adicione um valor para ver a sua tabuada: '))
+for c in range(1, 11):
+    input('{} x {} = {}'.format(x, c, x*c))
+>>>>>>> 740019a2dbb68d18689863dbaf0110e4f2edcc99
