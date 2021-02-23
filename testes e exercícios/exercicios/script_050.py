ec2cb2a227ca384bee1d59a48a86a0e8d59007da
Luiz <eluiz8204@gmail.com>
2021-02-23 10:22:32 -0400

    Update script_050.py

--- testes e exercícios/exercicios/script_050.py
+++ testes e exercícios/exercicios/script_050.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 x = 0
 quant = 0
 for c in range(0, 6):
@@ -7,3 +8,14 @@ for c in range(0, 6):
         quant += 1
 
 print('O total de pares são: {}\nE a soma é: {}'.format(quant, x))
+=======
+x = 0
+quant = 0
+for c in range(0, 6):
+    valor = int(input())
+    if(valor % 2 == 0):
+        x += valor
+        quant += 1
+
+print('O total de pares são: {}\nE a soma é: {}'.format(quant, x))
+>>>>>>> 740019a2dbb68d18689863dbaf0110e4f2edcc99
