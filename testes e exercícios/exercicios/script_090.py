71cf36bc9b7bc1a004a9bd69445ba229c448a52f
Luiz <eluiz8204@gmail.com>
2022-05-16 18:25:25 -0400

    Update script_090.py

--- testes e exercícios/exercicios/script_090.py
+++ testes e exercícios/exercicios/script_090.py
@@ -6,6 +6,8 @@ aluno['nome'] = str(input('Digite o nome: '))
 aluno['media'] = float(input(f"Digite a média de {aluno['nome']}: "))
 if(aluno['media'] > 6):
     aluno['situa'] = 'Aprovado'
+elif aluno['media'] >= 5 and aluno['media'] < 7:
+    aluno['situa'] = 'Recuperação'
 else:
     aluno['situa'] = 'Reprovado'
 print(f"A média é igual a {aluno['media']:.2f}")
